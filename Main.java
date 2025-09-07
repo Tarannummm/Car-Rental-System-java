ba6acda7c2683db14850fa4a453f9a5ca3e1364f
Tarannummm <tarannumelina@gmail.com>
2025-09-07 22:44:03 +0600

    started the menu

--- Main.java
+++ Main.java
@@ -59,8 +59,6 @@ class Rental{
      private Customer customer;
      private int days;
 
-
-
      public Rental (Car car, Customer customer, int days){
         this.car=car;
         this.customer=customer;
@@ -76,24 +74,57 @@ class Rental{
      }
 }
 
-
-
-
-
 class CarRentalSystem{
+    private List<Car> cars;
+    private List<Customer> customers;
+    private List<Rental> rentals;
+    public CarRentalSystem(){
+        cars =new ArrayList<>();
+        customers =new ArrayList<>();
+        rentals =new ArrayList<>();
+    }
+    public void addCar(Car car){
+        cars.add(car);
+    }
+    public void addCustomer(Customer customer){
+        customers.add(customer);
+    }
+    public void rentCar(Car car, Customer customer,int days){
+        if (car.isAvailable()){
+            car.rent();
+            rentals.add(new Rental(car, customer, days));
+        }
+        else{
+            System.out.println("Car is not available");
+        }
+    }
+    public void returnCar(Car car){
+        car.returnCar();
+        Rental rentalToRemove =null;
+        for (Rental rental: rentals){
+            if(rental.getCar()==car){
+                rentalToRemove=rental;
+                break;
+            }
+        }
+        if( rentalToRemove !=null){
+            rentals.remove(rentalToRemove);
+        }
+        else{
+            System.out.println("Car was not rented");
+        }
+    }
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
 
-}
+        while (true) {
+            System.out.println("===== Car Rental System =====");
+            System.out.println("1. Rent a Car");
+            System.out.println("2. Return a Car");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
 
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
 
-
-
-
-
-
-
-
-
-
-public class Main {
-    
-}
+           
