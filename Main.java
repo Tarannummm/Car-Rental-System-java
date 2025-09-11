0dc0a1633235bba9cea7bafedcc224dd7954bfa7
Tarannummm <tarannumelina@gmail.com>
2025-09-11 23:14:24 +0600

    new car added

--- Main.java
+++ Main.java
@@ -224,12 +224,14 @@ public class Main{
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = new CarRentalSystem();
 
-        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
-        Car car2 = new Car("C002", "Honda", "Accord", 70.0);
+        Car car1 = new Car("C001", "Toyota", "Camry", 50.0); 
+        Car car2 = new Car("C002", "Honda", "Accord", 90.0);
         Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+        Car car4 = new Car("C004", "Toyota", "Allion", 200.0);
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
+        rentalSystem.addCar(car4);
 
         rentalSystem.menu();
     }
